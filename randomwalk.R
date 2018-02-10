6a7906cbb5da95f3268a8806db248547d67fb56a
Shresth Verma <vermashresth@gmail.com>
2018-02-11 03:27:13 +0530

    algo

--- randomwalk.R
+++ randomwalk.R
@@ -1,6 +1,31 @@
-#!/usr/bin/env Rscript
-sayHello <- function(){
-   print('hello')
+lg = function(mu, n, ybar) { 
+	mu2 = mu^2
+	n * (ybar * mu -mu2 / 2.0) - log(1.0 +mu2)
+
 }
 
-sayHello()
+mh = function(n, ybar, n_iter, mu_init, cand_sd){
+	mu_out = numeric(n_iter)
+	accpt = 0
+	mu_now = mu_init
+	lg_now = lg(mu_now, n=n,ybar=ybar)
+
+	for (i in 1:n_iter){
+		mu_cand=rnorm(1,mean=mu_now, sd=cand_sd)
+
+		lg_cand=lg(mu=mu_cand, n-n, ybar=ybar)
+		lalpha=lg_cand - lg_now
+		alpha = exp(lalpha)
+
+		u=runif(1)
+		if(u<alpha){
+			mu_now=mu_cand
+			accpt=accpt+1
+			lg_now=lg_cand
+		}
+
+		mu_out[i] = mu_now
+	}
+
+	list(mu=mu_out,accpt=accpt/n_iter)
+}
