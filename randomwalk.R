b0fd74f1ce99a0404c56f2910c3fedeb7a91b21f
Shresth Verma <vermashresth@gmail.com>
2018-02-11 04:23:20 +0530

    plots

--- randomwalk.R
+++ randomwalk.R
@@ -29,3 +29,11 @@ mh = function(n, ybar, n_iter, mu_init, cand_sd){
 
 	list(mu=mu_out,accpt=accpt/n_iter)
 }
+
+y=c(1.2,1.4,-0.5,0.3,0.9,2.3,1.0,0.1,1.3,1.9)
+
+ybar=mean(y)
+n=length(y)
+print(n)
+hist(y,freq=FALSE, xlim=c(-1.0,3.0))
+
